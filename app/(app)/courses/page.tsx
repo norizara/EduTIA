50c946c36a4a3ed1a06ab78d99cc4709aeb4ff80
oliviathe <oliviathe102@gmail.com>
2026-01-14 15:00:14 +0700

    add courses page and filter, connect to landing page

--- app/(app)/courses/page.tsx
+++ app/(app)/courses/page.tsx
@@ -1,7 +1,26 @@
-export default function Page() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+import { prisma } from "@/lib/prisma";
+import Courses from "@/components/Courses";
+
+export const dynamic = "force-dynamic";
+
+export default async function Page() {
+  const courses = await prisma.course.findMany({
+    where: {
+      isPublished: true,
+    },
+    include: {
+      category: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const categories = await prisma.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  
+  return <Courses courses={courses} categories={categories} />;
 }
